20cf9be9d6418d3f30b9ffce29bf945bcbd0198b
Umararshad Tanoli <umar.arshad.tanoli@snl.int>
2020-01-27 04:40:07 +0500

    Updates

---
+++ precache-manifest.091d3de41693563fb1592339baa66977.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8fb964e2483263a17fe96358f089a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc999a37436588ba08a8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fc999a37436588ba08a8",
+    "url": "/static/js/main.cbf62320.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
